b0a5ab83dfe50ad45e2c975c10f49128386a6130
Wisdom Okogho <91355531+setgram@users.noreply.github.com>
2023-06-05 00:59:53 +0100

    sort the sign in, sign up, forgot password, page

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
           <ul className="flex space-x-10">
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRoute("/") && "text-black border-b-red-500"
+                pathMathRoute("/") && "text-black border-b-green-500"
               }`}
               onClick={() => navigate("/")}
             >
@@ -32,7 +32,7 @@ export default function Header() {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRoute("/offers") && "text-black border-b-red-500"
+                pathMathRoute("/offers") && "text-black border-b-green-500"
               }`}
               onClick={() => navigate("offers")}
             >
@@ -40,7 +40,7 @@ export default function Header() {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRoute("/sign-in") && "text-black border-b-red-500"
+                pathMathRoute("/sign-in") && "text-black border-b-green-500"
               }`}
               onClick={() => navigate("/sign-in")}
             >
