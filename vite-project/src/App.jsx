8f04fe2dfec5abdcd335f923362c558cc1bb1071
Wisdom Okogho <91355531+setgram@users.noreply.github.com>
2023-06-07 09:23:36 +0100

    install react js, tailwind css

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,28 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Offers from "./pages/Offers";
-import SignIn from "./pages/Sign-In";
-import SignUp from "./pages/Sign-Up";
-import Profile from "./pages/Profile";
-import ForgetPassword from "./pages/ForgotPassword";
-import Header from "./components/Header";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/forgot-password" element={<ForgetPassword />} />
-          <Route path="/offers" element={<Offers />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <h1 className="text-left border-l-8">Hello</h1>
+      <p>I love you</p>
+    </>
   );
 }
 
