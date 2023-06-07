b2294d7b493b60d3cfc96b3c78e77336cd9e6d57
Wisdom Okogho <91355531+setgram@users.noreply.github.com>
2023-06-07 09:52:42 +0100

    pages and route

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
     <>
-      <h1 className="text-left border-l-8">Hello</h1>
-      <p>I love you</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
